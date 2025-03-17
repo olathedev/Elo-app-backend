4622feade3320518fdf073c3b4b0a4c55bf399c8
olathedev <olapademicheal3@gmail.com>
2025-03-18 00:40:20 +0100

    initial scaffold 🚀

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config;
+
+const app: Application = express();
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import { createServer } from 'http';
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    const httpServer = createServer(app);
+
+    const server = httpServer.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+
+    process.on('unhandledRejection', (err) => {
+      console.log('UNHANDLED REJECTION! Shutting down...');
+      console.error(err);
+      server.close(() => {
+        process.exit(1);
+      });
+    });
+    
+  } catch (error) {
+    console.log('Error starting server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
